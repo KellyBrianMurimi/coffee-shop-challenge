43c001d9180fc5cd73db8cfc0917f748fd0144b4
KellyBrianMurimi <kellybrianmurimi@gmail.com>
2025-05-18 20:48:07 +0300

    debug.py

--- debug.py
+++ debug.py
@@ -14,3 +14,9 @@ order3 = beth.create_order(latte, 6.0)
 print(f"{kelly.name} ordered {[c.name for c in kelly.coffees()]}")
 print(f"Total orders for Latte: {latte.num_orders()}")
 print(f"Average price of Latte: {latte.average_price():.2f}")
+
+aficionado = Customer.most_aficionado(latte)
+if aficionado:
+    print(f"The most aficionado for {latte.name} is {aficionado.name}.")
+else:
+    print(f"No aficionado found for {latte.name}.")
