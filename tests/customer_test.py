70ddde138e49a8860486c64f59b2e3c7ebecb9d2
KellyBrianMurimi <kellybrianmurimi@gmail.com>
2025-05-19 00:38:18 +0300

    Tests

--- tests/customer_test.py
+++ tests/customer_test.py
@@ -1,16 +1,12 @@
 import unittest
 from customer import Customer
-from coffee import Coffee
 
 class TestCustomer(unittest.TestCase):
     def test_name_validation(self):
         with self.assertRaises(ValueError):
-            Customer()
-            with self.assertRaises(ValueError):
-                Customer(name="K" * 20)
+            Customer(name="K" * 20)  
         c = Customer(name="Kelly")
         self.assertEqual(c.name, "Kelly")
 
 if __name__ == "__main__":
     unittest.main()
-    
